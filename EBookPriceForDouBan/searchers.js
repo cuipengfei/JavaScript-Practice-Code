674be6cc452721cf5552eeb3788d6e2086afe325
cuipengfei <cuipengfei2008@gmail.com>
2013-02-07 17:37:49 +0800

    improve searching accuracy for 多看。

--- EBookPriceForDouBan/searchers.js
+++ EBookPriceForDouBan/searchers.js
@@ -1,13 +1,15 @@
 function SearchResult(provider, price, url) {
-    this.provider = provider;
     this.price = price;
     this.url = url;
     this.description = provider + "： " + price + "元";
 }
 
 var duokanSearcher = {
-    search:function (title, searchResults) {
-        var duokanSearchUrl = "http://book.duokan.com/store/v0/web/search?s=" + title;
+    search:function (title, subhead, author, searchResults) {
+        var duokanSearchUrl = "http://book.duokan.com/store/v0/web/search?s="
+            + title
+//            + (subhead.length > 0 ? "：" + subhead : "")
+            + (author.length > 0 ? " " + author : "");
         var duokanBookUrlTemplate = "http://book.duokan.com/dkdetail.html?book_id=";
 
         //todo:use async call.
@@ -16,8 +18,10 @@ var duokanSearcher = {
         $.ajax({url:duokanSearchUrl, async:false, success:function (data) {
             var response = $.parseJSON(data);
             if (response.items.length > 0) {
-                var url = duokanBookUrlTemplate + response.items[0].book_id;
-                var price = response.items[0].new_price ? response.items[0].new_price : response.items[0].price;
+                var firstMatch = response.items[0];
+                var url = duokanBookUrlTemplate + firstMatch.book_id;
+                var price = firstMatch.new_price ? firstMatch.new_price : firstMatch.price;
+
                 var duokanSearchResult = new SearchResult("多看阅读", price, url);
                 searchResults.push(duokanSearchResult);
             }
@@ -30,12 +34,53 @@ var searchers = [duokanSearcher];
 function searchForEBooks() {
     var searchResults = [];
 
-    var titleSpan = jQuery("#wrapper > h1 > span");
-    var bookTitle = titleSpan.text();
+    var bookTitle = getTitle();
+    var subhead = getSubHead();
+    var author = getAuthor();
 
     $.each(searchers, function (index, searcher) {
-        searcher.search(bookTitle, searchResults);
+        //we give all three parameters to every searcher, it's up to the searcher to decide whether to use them
+        //or ignore them.
+        searcher.search(bookTitle, subhead, author, searchResults);
     });
 
     return searchResults;
+}
+
+function getSubHead() {
+    var clonedDiv = $("#info").clone();
+    if (clonedDiv.text().indexOf("副标题") === -1) {
+        return "";
+    }
+
+    clonedDiv.children().remove();
+    var text = clonedDiv.text();
+
+    var textArray = text.split("\n");
+
+    var subHead = textArray.map(function (item) {
+        return item.trim();
+    }).filter(function (item) {
+            return item.length > 0;
+        })[0];
+
+    return subHead === undefined ? "" : subHead;
+}
+
+function getTitle() {
+    var titleSpan = $("#wrapper > h1 > span");
+    var bookTitle = titleSpan.text();
+    return bookTitle;
+}
+
+function getAuthor() {
+    var authorLink = $("#info > span > a");
+    var href = authorLink.attr("href");
+
+    if (!href) {
+        return "";
+    }
+
+    var authorNameEncoded = href.split("/")[2];
+    return decodeURIComponent(authorNameEncoded ? authorNameEncoded : "");
 }
