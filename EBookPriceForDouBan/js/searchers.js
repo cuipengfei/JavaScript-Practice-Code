77a0fafd7d5b01db5fb5314177d49f2cd4616749
cuipengfei <cuipengfei2008@gmail.com>
2013-02-16 10:43:29 +0800

    Use douban API instead of analyzing html. I was an idiot :(
    Even though analyzing taobao and jingdong html pages is the right call, since their APIs are so ballache.

--- EBookPriceForDouBan/js/searchers.js
+++ EBookPriceForDouBan/js/searchers.js
@@ -11,67 +11,21 @@ var searchers = [duokanSearcher, tangchaSearcher, amazonSearcher, taobaoSearcher
 function searchForEBooks() {
     var searchResults = ko.observableArray();
 
-    var bookTitle = getTitle();
-//    var subhead = getSubHead();
-    var author = getAuthor();
-    var isbn = getISBN();
+    var urlFragments = location.pathname.split("/");
+    var bookID = urlFragments[urlFragments.length - 2];
 
-    $.each(searchers, function (index, searcher) {
-        //we give all parameters to every searcher, it's up to the searcher to decide whether to use them
-        //or ignore them.
-        searcher.search(
-            {
-                title:bookTitle,
-//                subhead:subhead,
-                author:author,
-                isbn:isbn,
-                searchResults:searchResults
-            });
-    });
+    $.ajax({url:"https://api.douban.com/v2/book/" + bookID, success:function (data) {
+        var jsonResult = data;
+
+        var bookTitle = jsonResult.title;
+        var author = jsonResult.author[0];
+        var isbn = jsonResult.isbn13;
+
+        $.each(searchers, function (index, searcher) {
+            //we give all parameters to every searcher, it's up to the searcher to decide whether to use them or ignore them.
+            searcher.search({title:bookTitle, author:author, isbn:isbn, searchResults:searchResults});
+        });
+    }});
 
     return searchResults;
-}
-
-function getSubHead() {
-    var div = $("#info");
-    if (div.text().indexOf("副标题") === -1) {
-        return "";
-    }
-
-    var subHead = getInfoTextArray()[0];
-    return subHead === undefined ? "" : subHead;
-}
-
-function getTitle() {
-    var titleSpan = $("#wrapper > h1 > span");
-    var bookTitle = titleSpan.text();
-    return bookTitle;
-}
-
-function getAuthor() {
-    var authorLink = $("#info > span > a");
-    var href = authorLink.attr("href");
-
-    if (!href) {
-        return "";
-    }
-
-    var authorNameEncoded = href.split("/")[2];
-    return decodeURIComponent(authorNameEncoded ? authorNameEncoded : "");
-}
-
-function getISBN() {
-    var textArray = getInfoTextArray();
-    return textArray[textArray.length - 1];
-}
-
-function getInfoTextArray() {
-    var clonedDiv = $("#info").clone();
-    clonedDiv.children().remove();
-    var textArray = clonedDiv.text().split("\n");
-    return   textArray.map(function (text) {
-        return text.trim();
-    }).filter(function (text) {
-            return text.length > 0;
-        });
 }
