d81830dc9caa25d3829cfcee4301b1d42adcfc23
cuipengfei <cuipengfei2008@gmail.com>
2013-02-18 13:18:55 +0800

    indentation

--- EBookPriceForDouBan/js/injecter.js
+++ EBookPriceForDouBan/js/injecter.js
@@ -3,14 +3,13 @@ function injectEmptyTemplate() {
         '<ul data-bind="foreach: searchResults">' +
             '<li>' +
                 '<a target="_blank" data-bind="attr: {href: url}">' +
-                '<div data-bind="if: isPriceInNumber">' +
-                    '<span data-bind="text: description"></span>' +
-                '</div>' +
-                '<div data-bind="ifnot: isPriceInNumber">' +
-                    '<span data-bind="text: provider">' +
-                    '</span>' +
-                    '<img data-bind="attr: {src: price}"/>' +
-                '</div>' +
+                    '<div data-bind="if: isPriceInNumber">' +
+                        '<span data-bind="text: description" />' +
+                    '</div>' +
+                    '<div data-bind="ifnot: isPriceInNumber">' +
+                        '<span data-bind="text: provider" />' +
+                        '<img data-bind="attr: {src: price}"/>' +
+                    '</div>' +
                 '</a>' +
             '</li>' +
         '</ul>';
