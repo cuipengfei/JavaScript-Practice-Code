c4efdc1c5fda9cd0f91e4b7ce6051c9c3c4de3b6
cuipengfei <cuipengfei2008@gmail.com>
2013-02-23 23:45:27 +0800

    extract google api calling

--- EBookPriceForDouBan/js/searchers/koboSearcher.js
+++ EBookPriceForDouBan/js/searchers/koboSearcher.js
@@ -1,35 +1,43 @@
 var koboSearcher = {
     search:function (searchParameter) {
         var searchResults = searchParameter.searchResults;
-        var googleBookSearchUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchParameter.title + "+inauthor:" + searchParameter.author;
+        var title = searchParameter.title;
+        var author = searchParameter.author;
 
-        $.ajax({url:googleBookSearchUrl, async:true, success:function (googleResultJson) {
-            var matchingISBNs = googleResultJson.items.filter(function (item) {
-                return item.volumeInfo.title === searchParameter.title;
-            }).map(function (item) {
-                    return item.volumeInfo.industryIdentifiers[1].identifier;
-                });
+        function searchWithMatchingISBN(matchingISBN) {
+            var koboSearchUrl = "http://www.kobobooks.com/search/search.html?q=" + matchingISBN;
 
-            if (matchingISBNs.length > 0) {
-                $.each(matchingISBNs, function (idx, matchingISBN) {
-                    var koboSearchUrl = "http://www.kobobooks.com/search/search.html?q=" + matchingISBN;
+            $.ajax({url:koboSearchUrl, async:true, success:function (data) {
+                var page = $(data);
+                var notFoundMsg = page.find("ctl00_ctl00_ctl00_Body_Body_ContentBody_liNoResults");
+                var priceTag = page.find(".KV2OurPrice");
 
-                    $.ajax({url:koboSearchUrl, async:true, success:function (data) {
-                        var page = $(data);
-                        var notFoundMsg = page.find("ctl00_ctl00_ctl00_Body_Body_ContentBody_liNoResults");
-                        var priceTag = page.find(".KV2OurPrice");
+                if (notFoundMsg.length === 0 && priceTag.length > 0) {
+                    var price = priceTag.find("strong").text().replace("$", "");
+                    var url = koboSearchUrl;
 
-                        if (notFoundMsg.length === 0 && priceTag.length > 0) {
-                            var price = priceTag.find("strong").text().replace("$", "");
-                            var url = koboSearchUrl;
+                    var koboSearchResult = new SearchResult("Kobo电子书", price, url, "美元");
+                    searchResults.push(koboSearchResult);
+                }
+            }});
+        }
 
-                            var koboSearchResult = new SearchResult("Kobo电子书", price, url, "美元");
-                            searchResults.push(koboSearchResult);
-                        }
-                    }});
-                });
+        function getMatchingISBNsFromGoogle(title, author, callBack) {
+            var googleBookSearchUrl = "https://www.googleapis.com/books/v1/volumes?q=" + title + "+inauthor:" + author;
+            $.ajax({url:googleBookSearchUrl, async:true, success:function (googleResultJson) {
+                var matchingISBNs = googleResultJson.items.filter(function (item) {
+                    return item.volumeInfo.title === title;
+                }).map(function (item) {
+                        return item.volumeInfo.industryIdentifiers[1].identifier;
+                    });
+                if (matchingISBNs.length > 0) {
+                    $.each(matchingISBNs, function (idx, matchingISBN) {
+                        callBack(matchingISBN);
+                    });
+                }
+            }});
+        }
 
-            }
-        }});
+        getMatchingISBNsFromGoogle(title, author, searchWithMatchingISBN);
     }
 };
