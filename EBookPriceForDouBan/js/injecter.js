72cc28d3609eec844784814be0d8939e92bca3fb
cuipengfei <cuipengfei2008@gmail.com>
2015-04-01 21:30:21 +0800

    求打赏

--- EBookPriceForDouBan/js/injecter.js
+++ EBookPriceForDouBan/js/injecter.js
@@ -1,18 +1,26 @@
 function injectEmptyTemplate() {
     var htmlTemplate = '<div>' +
         '<ul data-bind="foreach: searchResults">' +
+
         '<li>' +
         '<a target="_blank" data-bind="attr: {href: url}">' +
+
         '<div data-bind="if: isPriceInNumber">' +
         '<span data-bind="text: description" />' +
         '</div>' +
+
         '<div data-bind="ifnot: isPriceInNumber">' +
         '<span data-bind="text: provider" />' +
         '<img data-bind="attr: {src: price}"/>' +
         '</div>' +
+
         '</a>' +
         '</li>' +
-        '</ul>';
+
+        '</ul>' +
+        '<li>支付宝扫码，打赏作者</li>' +
+        '<img src="https://tfsimg.alipay.com/images/mobilecodec/T1J3FfXbpsXXXXXXXX">' +
+        '</div>';
 
     $(".gray_ad").first().append($(htmlTemplate));
 }
